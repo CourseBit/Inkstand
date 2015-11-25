0090a2f125b3d60897f107b3affc167b37ef8753
Joe Conradt <joe@conradtweb.com>
2015-11-24 21:15:52 -0600

    #17 Add voter for roles, and add list view

---
+++ src/Inkstand/Bundle/CoreBundle/Voter/RoleVoter.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Inkstand\Bundle\CoreBundle\Voter;
+
+use Inkstand\Bundle\CoreBundle\Voter\AbstractVoter;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+class RoleVoter extends AbstractVoter
+{
+    const ADD = 'add';
+    const VIEW = 'view';
+    const EDIT = 'edit';
+    const DELETE = 'delete';
+
+    public function getSupportedAttributes()
+    {
+        return array(self::ADD, self::VIEW, self::EDIT, self::DELETE);
+    }
+
+    public function getDefaultRoleAssignments()
+    {
+        return array(
+            self::ADD => 'ROLE_ADMIN',
+            self::VIEW => 'ROLE_STUDENT',
+            self::EDIT => 'ROLE_ADMIN',
+            self::DELETE => 'ROLE_ADMIN'
+        );
+    }
+
+    protected function getSupportedClasses()
+    {
+        return array('Inkstand\Bundle\CoreBundle\Entity\Role');
+    }
+
+    protected function isGranted($attribute, $role, $user = null)
+    {
+        $user->getRoles();
+
+        foreach($user->getRoles() as $roleName) {
+            if($this->isRoleGranted($roleName, $attribute)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    protected function isRoleGranted($roleName, $actionName)
+    {
+        return $this->voterService->isRoleGranted($roleName, get_class($this), $actionName);
+    }
+}
